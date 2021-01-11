fd1e042a43377cc5eedc42d1f26033c118afad9c
WhoIAm451 <74014201+WhoIAm451@users.noreply.github.com>
2021-01-11 11:17:48 +0100

    Update avatar.js

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -1,6 +1,4 @@
-const { MessageEmbed } = require("discord.js");
-const { readdirSync } = require("fs");
-const prefixSchema = require('../../models/prefix')
+const Discord = require("discord.js");
 
 module.exports = {
   name: "avatar",
@@ -8,21 +6,35 @@ module.exports = {
   description: 'i will send your avatar or the avatar of the member.',
   timeout: 1000,
   run: async (client, message, args) => {
-    let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member || message.author;
-
-    if (!member && args.slice().length === 0) {
-      member = message.author;
-    }
-    else if (member) {
-      if (member.bot) return message.channel.send(member.displayAvatarURL( {format: 'png', dynamic: true, size: 4096} ));
-    }
-    else {
-        const fetchedMember = await message.guild.members.fetch(args.slice().join(' '));
-        if (!fetchedMember) new Error('Utilisateur non trouver!');
-        user = fetchedMember;
-        user = user.user;
-    }
+        try{
+          if (!message.channel.permissionsFor(message.guild.me).has("ATTACH_FILES")) return message.channel.send(`I don't have permissions to attach files to my messages.`);
   
-    message.channel.send(user.displayAvatarURL( {format: 'png', dynamic: true, size: 4096} ))
+          let user = message.mentions.users.first();
+  
+          if (!user) {
+              const input = message.content.split(` `, 2);
+              let userID = input[1];
+              user = client.users.cache.get(userID);
+          };
+  
+          if (!user) {
+              user = message.author;
+          };
+  
+          let totalMessage = `${user.tag}'s avatar.`;
+  
+          let avatar = null;
+          if (user.avatarURL()) avatar = user.avatarURL({ format: "png", dynamic: true, size: 4096 });
+          if (!avatar) return message.channel.send(`${user.tag} doesn't have an avatar, ${message.author}.`);
+  
+          return message.channel.send(totalMessage, {
+              files: [avatar]
+          });
+  
+    //message.channel.send(user.displayAvatarURL( {format: 'png', dynamic: true, size: 4096} ))
+  } catch(err) {
+    console.error(err)
+    return message.channel.send(error)
+}
     }
 }
